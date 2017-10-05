f98f4cc76682b10350431b0c1a6fc7f75eff4deb
Nicole Campbell <nscampbell303@gmail.com>
2017-10-05 21:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
-  if (line.push(`${line}`)) {
-    return `The line is currently: ${line.length}`
+  if (line.length > 0) {
+    var newArray = []
+    for (var i = 0; i < line.length; i++) {
+      // 1. Bill
+      newArray.push(`${i + 1}. ${line[i]}`)
+    }
+    // newArray's value is ['1. Bill', '2. Jane', '3. Ann']
+    return `The line is currently: ${newArray}`
   } else {
     return "The line is currently empty."
   }
